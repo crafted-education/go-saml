ef78d5bdb4c135de7fc3bd795fa1926b03110e4f
Matt Self <self.matt@gmail.com>
2020-02-25 09:01:42 -0700

    had the wrong xsd algorithm location from w3.org, fixing in saml authnrequest now

--- authnrequest.go
+++ authnrequest.go
@@ -151,7 +151,7 @@ func NewAuthnRequest() *AuthnRequest {
 					XMLName: xml.Name{
 						Local: "ds:SignatureMethod",
 					},
-					Algorithm: "http://www.w3.org/2000/09/xmldsig#rsa-sha256",
+					Algorithm: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
 				},
 				SamlsigReference: SamlsigReference{
 					XMLName: xml.Name{
@@ -178,7 +178,7 @@ func NewAuthnRequest() *AuthnRequest {
 						XMLName: xml.Name{
 							Local: "ds:DigestMethod",
 						},
-						Algorithm: "http://www.w3.org/2000/09/xmldsig#sha256",
+						Algorithm: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
 					},
 					DigestValue: DigestValue{
 						XMLName: xml.Name{
